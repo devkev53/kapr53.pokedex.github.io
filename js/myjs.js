58a728ac757fbdc562cabebcedf1766c583161b9
Kevin Palma <kapalma05@gmil.com>
2021-04-14 20:01:40 -0600

    Se modificaron los estilos css del los distintos tamaños de dispositivos, y se modifico parte de la cabecera, se le dio un acabado con animacion en el hover

--- js/myjs.js
+++ js/myjs.js
@@ -2,5 +2,13 @@ function showMenu() {
     var menu = document.querySelector('.menu_list');
     if (menu){
         menu.classList.toggle("show");
+        var formSearchPK = document.querySelector('.form_search_pk');
+        if (menu.classList.contains("show")) {
+            formSearchPK.style.margin = "100rem 0 0 0";
+            console.log("Se puso display none")
+        } else {
+            formSearchPK.style.margin = "0 0 0 0";
+            console.log("Se puso display flex")
+        }
     }
 }
